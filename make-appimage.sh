9f10a8c860a0e4c9f08482c20c436021dfb316ce
Link4Electronics <link4electronics@gmail.com>
2026-01-23 21:11:24 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/pixmaps/rednukem.ico
 export DESKTOP=/usr/share/applications/rednukem.desktop
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/rednukem
