70927907deb48646020b3fc3ccf161180d7d29ae
Link4Electronics <link4electronics@gmail.com>
2026-02-03 09:39:13 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,13 +3,12 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q rednukem-git | awk '{print $2; exit}') # example command to get version of application here
+#VERSION=$(pacman -Q rednukem-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/rednukem.ico
-export DESKTOP=/usr/share/applications/rednukem.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
