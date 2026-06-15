d5abafff116c37e8b07aebb965d3561a9da86e71
Link4Electronics <link4electronics@gmail.com>
2026-06-15 00:31:43 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,29 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    glu      \
-    libdecor \
-    libvpx   \
-    sdl2
+pacman -Syu --noconfirm glu libvpx sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano gtk2-mini libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package gtk2
-
-# If the application needs to be manually built that has to be done down here
-echo "Making nightly build of Rednukem..."
+echo "Building Rednukem..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/NBlood/NBlood"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-git clone "$REPO" ./Rednukem
+git clone --depth 1 "$REPO" ./Rednukem
 echo "$VERSION" > ~/version
 
+mkdir -p ./AppDir/bin
 cd ./Rednukem
 make rednukem -j$(nproc)
-mv -v rednukem /usr/bin
-mkdir -p /usr/share/games/rednukem
-mv -v dn64widescreen.pk3 /usr/share/games/rednukem
+mv -v rednukem dn64widescreen.pk3 ../AppDir/bin
