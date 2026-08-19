891e118a99c79fa5f7394fb7c7d1cbd3c0d8755b
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:05:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,7 +12,5 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun /usr/bin/rednukem
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
